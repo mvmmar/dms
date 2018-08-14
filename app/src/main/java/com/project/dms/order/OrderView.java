08fbe387eedc61fca9e6bcca433a57975497c0df
muammar <muammararif007@gmail.com>
2018-08-14 22:38:27 +0530

    update

--- app/src/main/java/com/project/dms/order/OrderView.java
+++ app/src/main/java/com/project/dms/order/OrderView.java
@@ -1,12 +1,19 @@
 package com.project.dms.order;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
@@ -15,6 +22,8 @@ import android.widget.TextView;
 
 import com.project.dms.R;
 import com.project.dms.product.Product;
+import com.project.dms.product.ProductMain;
+import com.project.dms.product.ProductUpdate;
 
 public class OrderView extends AppCompatActivity {
 
@@ -28,56 +37,83 @@ public class OrderView extends AppCompatActivity {
         Order order = intent.getParcelableExtra("Order");
         this.setTitle(order.name);
 
-        Product[] products = {
-                new Product("Product","product Description",1.23,2),
-                new Product("Product","product Description",4.49,1),
-                new Product("Product","product Description",9.99,4),
-                new Product("Product","product Description",19.99,1),
-                new Product("Product","product Description",1.25,3),
-                new Product("Product","product Description",1.30,1),
-                new Product("Product","product Description",3.99,2),
-                new Product("Product","product Description",2.49,1),
+        Product[] products;
+        Product[] products1 = {
+                new Product("Product", "product Description", 1.23, 2),
+                new Product("Product", "product Description", 4.49, 1),
+                new Product("Product", "product Description", 9.99, 4),
+                new Product("Product", "product Description", 19.99, 1),
+                new Product("Product", "product Description", 1.25, 3),
+                new Product("Product", "product Description", 1.30, 1),
+                new Product("Product", "product Description", 3.99, 2),
+                new Product("Product", "product Description", 2.49, 1),
         };
 
-        double productsTotal = 0;
-        double tax = 0.1;
-        double totalTax = 0;
+        Product[] products2 = {
+                new Product("Product", "product Description", 2.49, 2),
+                new Product("Product", "product Description", 10.99, 1),
+                new Product("Product", "product Description", 9.99, 5),
+                new Product("Product", "product Description", 1.25, 3),
+                new Product("Product", "product Description", 1.30, 1),
+                new Product("Product", "product Description", 3.99, 2),
+        };
 
-        if (products != null) {
-            ListAdapter orderAdapter = new OrderViewProductAdapter(this, products);
-            ListView orderList = findViewById(R.id.orderViewProductsList);
-            orderList.setAdapter(orderAdapter);
-
-            for (Product product: products) {
-                productsTotal += product.getQuantity() + product.getPrice();
-            }
-        }
-        totalTax = tax * productsTotal;
+        Product[] products3 = {
+                new Product("Product", "product Description", 1.23, 2),
+                new Product("Product", "product Description", 4.49, 1),
+                new Product("Product", "product Description", 2.49, 1),
+                new Product("Product", "product Description", 4.49, 1),
+                new Product("Product", "product Description", 9.99, 4),
+        };
 
         TextView orderDate = findViewById(R.id.orderDeliveryDate);
         TextView orderStatus = findViewById(R.id.orderViewStatus);
         ImageView orderStatusIcon = findViewById(R.id.orderViewStatusIcon);
+        LinearLayout acceptButtonContainer = findViewById(R.id.orderViewAcceptDeny);
+        Button orderAcceptButton = findViewById(R.id.orderViewAcceptButton);
 
         int statusColor, statusIcon;
         switch (order.status) {
             case "Accepted": {
+                products = products1;
                 statusColor = R.color.orderStatusAccepted;
                 statusIcon = R.drawable.ic_status_accepted;
+                findViewById(R.id.orderViewDenyButton).setVisibility(View.GONE);
+                orderAcceptButton.setText("Cancel");
+                acceptButtonContainer.setVisibility(View.VISIBLE);
                 break;
             }
             case "Pending": {
+                products = products2;
                 statusColor = R.color.orderStatusPending;
                 statusIcon = R.drawable.ic_status_pending;
                 order.date = "--/--/--";
-                LinearLayout acceptButton = findViewById(R.id.orderViewAcceptDeny);
-                acceptButton.setVisibility(View.VISIBLE);
+                acceptButtonContainer.setVisibility(View.VISIBLE);
                 break;
             }
             default: {
+                products = products3;
                 statusColor = R.color.orderStatusDenied;
                 statusIcon = R.drawable.ic_status_denied;
             }
         }
+
+        double productsTotal = 0;
+        double tax = 0.1;
+        double totalTax;
+
+        if (products != null) {
+            // populate products
+            ListAdapter orderAdapter = new OrderViewProductAdapter(this, products);
+            ListView orderList = findViewById(R.id.orderViewProductsList);
+            orderList.setAdapter(orderAdapter);
+
+            for (Product product : products) {
+                // get total
+                productsTotal += product.getQuantity() + product.getPrice();
+            }
+        }
+        totalTax = tax * productsTotal;
 
         statusColor = ContextCompat.getColor(this, statusColor);
         orderDate.setText(order.date);
